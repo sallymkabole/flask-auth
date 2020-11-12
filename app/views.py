48a7961f4013740454fd565046c0b47c68a9455a
sallythetechie <sllmusanga7@gmail.com>
2020-11-12 20:27:17 +0300

    Add dashboard template, Add login session to enable user login

--- app/views.py
+++ app/views.py
@@ -1,15 +1,55 @@
-from flask import render_template
-
+from flask import render_template, request, redirect, url_for, session, escape
+from os import environ
 from app import app
-@app.route('/')
-def index():
-    return("Welcome")
 
-@app.route('/signup')
+#set my secret key
+app.secret_key = "ghaifafa"
+#create session object for log in
+users = {"sally": ("sally", "1234")}
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+
+        fullname = request.form.get("fullname")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        return redirect(url_for('dashboard'))
     return render_template("signup.html")
 
-
-@app.route('/login')
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if not username in users and users[username][1] == password:
+            print("Wrong Credentials")
+            return redirect('request.url')
+        else:
+            session["username"] = username
+            return redirect(url_for("dashboard"))
+
+        
+        
     return render_template("login.html")
+
+
+
+
+@app.route("/logout")
+def logout():
+    # remove the username from the session if it's there
+    session.pop('email', None)
+    return redirect(url_for('home'))
